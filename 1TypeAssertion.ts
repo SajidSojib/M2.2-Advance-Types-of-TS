74abe93800f4379fd79f5fe539f5169deea3058c
SajidSojib <sajidahmed2401@gmail.com>
2025-11-10 20:40:38 +0600

    type assertion

---
+++ 1TypeAssertion.ts
@@ -0,0 +1,30 @@
+//* Type Assertion -> mean to tell the compiler that i am sure about the type
+//* only use when you are absolutely sure about the type
+
+let anything: any;
+anything = 22;
+// anything.       //? doesnt recommend any method
+
+// (anything as number).   //* recommend number methoods
+
+
+const kgToGM = (kg: number | string) => {
+    if(typeof kg === 'number'){
+        return kg * 1000;
+    }
+    if(typeof kg === 'string'){
+        const numKG = kg.split(' ')[0];
+        return 'Number is ' + Number(numKG) * 1000;
+    }
+}
+
+const result1 = kgToGM(23) as number;      
+const result2 = kgToGM('23 kg');           //? string | number | undefined
+console.log(result1, result2);
+
+// result2.   //? doesnt recommend any method 
+// result1.   //? recommend number methoods
+
+
+
+
